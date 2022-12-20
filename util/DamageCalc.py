a0f6a78b3d71626b1de0d8ee16849fc733d7cbd6
oh-yes-0-fps <55303619+oh-yes-0-fps@users.noreply.github.com>
2022-12-20 18:41:04 +0000

    ye

--- util/DamageCalc.py
+++ util/DamageCalc.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, field
-from logging import CRITICAL
 from typing import Optional, Tuple
 from DataCurve import DataCurve, CurveKey
 # from util.DataCurve import DataCurve, CurveKey
